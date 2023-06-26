4092ee35b0ada5680b15b4ce057470d45ff76949
sorhawell <sorhawell@gmail.com>
2023-06-26 10:21:26 +0200

    %-print fail

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -4,6 +4,46 @@ use extendr_api::prelude::*;
 /// @export
 #[extendr]
 fn hello_world() -> &'static str {
+    rprintln!("\n\n print pct-char ");
+    //println!("\n123%321");
+    rprintln!("123%321");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print quoted pct-char ");
+    //println!("\n123'%'321");
+    rprintln!("123'%'321");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print inserted pct-char : ");
+    //println!("{}", "\n123%321");
+    rprintln!("123{}321", "%");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print quoted inserted pct-char : ");
+    //println!("{}", "\n123'%'321");
+    rprintln!("123{}321", "'%'");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print dollarsign-char ");
+    //println!("\n123$321");
+    rprintln!("123$321");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print quoted dollarsign-char ");
+    //println!("\n123'$'321");
+    rprintln!("123'$'321");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print inserted dollarsign-char : ");
+    //println!("\n123{}321", "$");
+    rprintln!("123{}321", "$");
+    rprintln!("print completed");
+
+    rprintln!("\n\n print quoted inserted dollarsign-char : ");
+    //println!("\n123{}321", "'$'");
+    rprintln!("123{}321", "'$'");
+    rprintln!("print completed");
+
     "Hello world!"
 }
 
